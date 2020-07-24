450d54492a11e03fd081b9c85993bca981b0967a
Vic Chimenti <chimenti@seattleu.edu>
2020-07-24 07:53:30 -0700

    ud

--- main-scripts.js
+++ main-scripts.js
@@ -2,4 +2,6 @@
 /**
  *  Override any active autofocus elements that are generated on window load
  */
-<script>{window.scrollTo(0, 0); }</script>
+<script>
+    { window.scrollTo(0, 0); }
+</script>
