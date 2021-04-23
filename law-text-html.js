1a6c5dec9e8975e838b8518e6453cf579d06dfee
Vic Chimenti <victor.chimenti@gmail.com>
2021-04-23 09:25:17 -0700

    ud zones

--- law-text-html.js
+++ law-text-html.js
@@ -3,7 +3,7 @@ try {
     var fieldFieldCode = content.get("Field Code");
     var fieldNoResultsMessage = content.get("No Results Message");
     var message = "";
-    // var fieldZone = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Zone' output='normal' display_field='value' />");
+    var fieldZone = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Zone' output='normal' display_field='value' />");
 
 
     if (fieldNoResultsMessage.isNull()) {
@@ -13,29 +13,23 @@ try {
     }
 
 
-    document.write('<div id="filterFieldWrapper" class="contentItem" data-posistion-default="ZoneA" data-position-selected="ZoneA" ><div id="filterField" class="standardContent">');
-    document.write('<div class="filterFieldInstructions">' + fieldInstructions + '</div>');
-    document.write('<div>' + fieldFieldCode + '<div class="clearfix"></div></div></div>');
-    document.write('<div class="noResultsToShow standardContent hideResultsMessage">' + message + '</div>');
-    document.write('</div>');
+    if (fieldZone == 'Main') {
 
-    // if (fieldZone == 'ZoneA') {
+        document.write('<div id="filterFieldWrapper" class="contentItem" data-posistion-default="Main" data-position-selected="Main" ><div id="filterField" class="standardContent">');
+        document.write('<div class="filterFieldInstructions">' + fieldInstructions + '</div>');
+        document.write('<div>' + fieldFieldCode + '<div class="clearfix"></div></div></div>');
+        document.write('<div class="noResultsToShow standardContent hideResultsMessage">' + message + '</div>');
+        document.write('</div>');
 
-    //     document.write('<div id="filterFieldWrapper" class="contentItem" data-posistion-default="ZoneA" data-position-selected="ZoneA" ><div id="filterField" class="standardContent">');
-    //     document.write('<div class="filterFieldInstructions">' + fieldInstructions + '</div>');
-    //     document.write('<div>' + fieldFieldCode + '<div class="clearfix"></div></div></div>');
-    //     document.write('<div class="noResultsToShow standardContent hideResultsMessage">' + message + '</div>');
-    //     document.write('</div>');
+    } else if (fieldZone == 'Sidebar') {
 
-    // } else if (fieldZone == 'ZoneB') {
+        document.write('<div id="filterFieldWrapper" class="contentItem" data-posistion-default="Sidebar" data-position-selected="Sidebar" ><div id="filterField" class="standardContent">');
+        document.write('<div class="filterFieldInstructions">' + fieldInstructions + '</div>');
+        document.write('<div>' + fieldFieldCode + '<div class="clearfix"></div></div></div>');
+        document.write('<div class="noResultsToShow standardContent hideResultsMessage">' + message + '</div>');
+        document.write('</div>');
 
-    //     document.write('<div id="filterFieldWrapper" class="contentItem" data-posistion-default="ZoneB" data-position-selected="ZoneB" ><div id="filterField" class="standardContent">');
-    //     document.write('<div class="filterFieldInstructions">' + fieldInstructions + '</div>');
-    //     document.write('<div>' + fieldFieldCode + '<div class="clearfix"></div></div></div>');
-    //     document.write('<div class="noResultsToShow standardContent hideResultsMessage">' + message + '</div>');
-    //     document.write('</div>');
-
-    // }
+    }
 
 
 } catch (err) {
