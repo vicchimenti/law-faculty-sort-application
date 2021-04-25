a0e08736b806a5a794da0cbb3b98722fc16ff101
Vic Chimenti <victor.chimenti@gmail.com>
2021-04-25 11:20:45 -0700

    ud remarks

--- law-organizer-faculty.js
+++ law-organizer-faculty.js
@@ -4,10 +4,6 @@
  *      @see Seattle University School of Law Faculty Profile Type
  *      law/organizer/facultyList/
  *
- *      This new content type layout is a smart layout for news items that must obey
- *      a masonry grid layout. In this iteration this layout will be dedicated
- *      to the faculty-staff bio content type.
- *
  *      This content layout will be the organizer layout and will link to the
  *      full text layout to reveal the full article.
  *
