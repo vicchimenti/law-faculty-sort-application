c046a6fd5594ca3638d3055d13486f595281475e
Vic Chimenti <victor.chimenti@gmail.com>
2021-04-25 12:40:11 -0700

    mt-0 all incarnations

--- law-organizer-masonryfeed.js
+++ law-organizer-masonryfeed.js
@@ -13,7 +13,7 @@
  *
  *      Document will write once when the page loads
  *
- *      @version 4.14
+ *      @version 4.15
  */
 
 
@@ -85,7 +85,7 @@
      * 
      * */
     if (biography != "") {
-        cardTitle = '<h3 class="card-title d-flex justify-content-center justify-content-md-start text-center text-md-start"><a href="' + fullTextLink + '" title="' + firstName + ' ' + lastName + ', ' + primaryTitle + '">' + firstName + ' ' + lastName + '</a></h3>';
+        cardTitle = '<h3 class="card-title d-flex justify-content-center justify-content-md-start text-center text-md-start mt-0"><a href="' + fullTextLink + '" title="' + firstName + ' ' + lastName + ', ' + primaryTitle + '">' + firstName + ' ' + lastName + '</a></h3>';
     }
 
 
