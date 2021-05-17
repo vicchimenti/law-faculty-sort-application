ead7846dd85771ef09615793b282424afb91246e
Vic Chimenti <victor.chimenti@gmail.com>
2021-05-17 09:02:14 -0700

    specify sareer switcher

--- law-organizer-career.js
+++ law-organizer-career.js
@@ -1,5 +1,5 @@
 /***
- *  law/organizer/suLawInTheNews
+ *  law/organizer/career
  * 
  */
 
@@ -8,9 +8,9 @@
  *  declare and assign topic layout
  * 
  */
- var fieldToBeEvaluated = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, '<t4 type="content" name="Category" output="normal" display_field="value" delimiter=";" />');
- var optionToTestFor = "suLawInTheNews"; //edit this to change the option
- var contentTypeLayout = 'output/suLawInTheNewsfeed'; //edit this to change the Content Layout to use for output
+ var fieldToBeEvaluated = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, '<t4 type="content" name="Faculty Status" output="normal" display_field="value" />');
+ var optionToTestFor = "Career"; //edit this to change the option
+ var contentTypeLayout = 'output/career'; //edit this to change the Content Layout to use for output
  var n = fieldToBeEvaluated.indexOf(optionToTestFor); /* determines starting character of string */
  
  
