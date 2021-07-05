6b37906d94301652542d232e08053faf506a0316
Vic Chimenti <victor.chimenti@gmail.com>
2021-07-05 10:46:33 -0700

    rm cm cd

--- law-organizer-masonryfeed.js
+++ law-organizer-masonryfeed.js
@@ -13,7 +13,7 @@
  *
  *      Document will write once when the page loads
  *
- *      @version 4.22
+ *      @version 4.24
  */
 
 
@@ -44,7 +44,6 @@
     var publications = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Publications' output='normal' modifiers='medialibrary,nav_sections' />");
     var activity = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Activity' output='normal' modifiers='medialibrary,nav_sections' />");
     var fullTextLink = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Name' output='fulltext' modifiers='striptags,htmlentities' />");
-    // var anchorTag = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='meta' meta='html_anchor' />");
     var contentID = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='meta' meta='content_id' />");
 
     
@@ -70,7 +69,6 @@
     var closeImageWrapper = '</div>';
     var openRow = '<div class="row g-0">';
     var closeRow = '</div>';
-    // var anchorWrap = '<div class="visually-hidden">' + anchorTag + '</div>';
     var openCard = '<div class="card h-100 w-100 border-0">'
     var closeCard = '</div>';
     var beginningHTML = '<div class="lawFacultyWrapper contentItem col flex-fill w-50" aria-label="' + firstName + ' ' + lastName + '" id="id' + contentID + '" data-position-default="Main" data-position-selected="Main">';
